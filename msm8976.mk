a0630b1abff59da4bcd1091f3f3aa67b54cdeeb1
ripee <ripee.dev@gmail.com>
2020-03-06 21:18:54 +0100

    msm8976-common: Convert lights HAL into a binderized HAL

--- msm8976.mk
+++ msm8976.mk
@@ -197,9 +197,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.MSM8952
+    android.hardware.light@2.0-service.samsung
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
