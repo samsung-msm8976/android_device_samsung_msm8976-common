e9ec38e5bd57367465963bef6c4d704517e6c7b8
ripee <ripee.dev@gmail.com>
2020-03-29 15:57:26 +0200

    Revert "msm8976-common: Add bootloader assert"
    This reverts commit 864759e373ac6b46b549cf6118456d1aa46a45ec.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,9 +195,6 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Release tools
-TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
-
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
