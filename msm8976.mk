fd211ae01565b39ec219498a9d197ba993e421d7
ripee <ripee.dev@gmail.com>
2020-04-23 00:03:49 +0200

    msm8976-common: Go back to passthrough camera implementation

--- msm8976.mk
+++ msm8976.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     camera.msm8952 \
     libshims_camera
 
